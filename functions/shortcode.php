269f6cd68981c93f14cdd0e5933c09f6faaf7bb8
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2020-05-05 10:50:34 +0000

    Add a Tag List ShortCode

--- functions/shortcode.php
+++ functions/shortcode.php
@@ -42,8 +42,32 @@ function sketchpad_copy_button( $atts ) {
   return $result;
 }
 
+// return tag list
+function sketchpad_tag_cloud( $atts ) {
+  $args = array(
+    'smallest'                  => 8,
+    'largest'                   => 22,
+    'unit'                      => 'pt',
+    'number'                    => 0,
+    'format'                    => 'list',
+    'separator'                 => "\n",
+    'orderby'                   => 'name',
+    'order'                     => 'ASC',
+    'exclude'                   => null,
+    'include'                   => null,
+    'topic_count_text_callback' => default_topic_count_text,
+    'link'                      => 'view',
+    'taxonomy'                  => 'post_tag',
+    'echo'                      => false,
+    'child_of'                  => null,
+  );
+
+  return wp_tag_cloud( is_array( $atts ) ? array_merge( $args, $atts ) : $args );
+}
+
 add_shortcode( 'sketchpad_get_current_url', 'sketchpad_get_current_url' );
 add_shortcode( 'sketchpad_get_feed_link', 'get_feed_link' );
 add_shortcode( 'sketchpad_has_tag', 'sketchpad_has_tag' );
 add_shortcode( 'sketchpad_get_avatar', 'sketchpad_get_avatar' );
 add_shortcode( 'sketchpad_copy_button', 'sketchpad_copy_button' );
+add_shortcode( 'sketchpad_tag_cloud', 'sketchpad_tag_cloud' );
