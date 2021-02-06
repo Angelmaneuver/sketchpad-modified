c89ad00e54d8f1f91144972ef3c0b8542e308f56
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-02-06 01:20:48 +0000

    Modified to comply with WordPress conventions and added theme support for align wide.

--- functions.php
+++ functions.php
@@ -2,47 +2,63 @@
 /**
  * Functions and definitions.
  *
- * @subpackage sketchpad
- * @since      1.0.0
+ * @package sketchpad
+ * @since 1.0.0
  */
 
 if ( ! isset( $content_width ) ) {
 	$content_width = 590;
 }
 
+/**
+ * WordPress initialize timing process.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/init/
+ */
 function sketchpad_init() {
-	/* This theme supports to category and tag the page. */
+	/** This theme supports to category and tag the page. */
 	register_taxonomy_for_object_type( 'category', 'page' );
 	register_taxonomy_for_object_type( 'post_tag', 'page' );
 }
 
+/**
+ * Theme setup process.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/after_setup_theme/
+ */
 function sketchpad_setup() {
-	/* Makes Sketchpad available for translation */
+	/** Makes Sketchpad available for translation. */
 	load_theme_textdomain( 'sketchpad-modified', get_template_directory() . '/languages' );
 
-	/* This theme support wp_nav_menu() in one location */
+	/** This theme support wp_nav_menu() in one location. */
 	register_nav_menu( 'primary', __( 'Primary menu', 'sketchpad-modified' ) );
 
-	/* This theme supports for post thumbnails */
+	/** This theme supports for post thumbnails. */
 	add_theme_support( 'post-thumbnails' );
 
-	/* Adds RSS feed links to <head> for posts and comments. */
+	/** This theme supports for wide alignment. */
+	add_theme_support( 'align-wide' );
+
+	/** Adds RSS feed links to <head> for posts and comments. */
 	add_theme_support( 'automatic-feed-links' );
 
-	/* This theme supports a variety of post formats */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'gallery',
-		'image',
-		'link',
-		'quote',
-		'status',
-		'video',
-		'audio',
-		'chat',
-	) );
+	/** This theme supports a variety of post formats. */
+	add_theme_support(
+		'post-formats',
+		array(
+			'aside',
+			'gallery',
+			'image',
+			'link',
+			'quote',
+			'status',
+			'video',
+			'audio',
+			'chat',
+		)
+	);
 
-	/* This theme supports custom logo image. */
+	/** This theme supports custom logo image. */
 	$args = array(
 		'width'              => 715,
 		'height'             => 117,
@@ -51,70 +67,117 @@ function sketchpad_setup() {
 		'header-text'        => true,
 		'default-image'      => get_template_directory_uri() . '/images/bg-logo.png',
 	);
+
 	add_theme_support( 'custom-header', $args );
 
-	/* This theme supports custom background color and image. */
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => 72,
+			'width'       => 162,
+			'flex-height' => false,
+			'flex-width'  => false,
+			'header-text' => array( 'site-title', 'site-description' ),
+		)
+	);
+
+	/** This theme supports custom background color and image. */
 	add_theme_support( 'custom-background', array( 'default-color' => 'AF9F88' ) );
 
+	/** This theme supports manage the document title tag. */
 	add_theme_support( 'title-tag' );
 
-	add_theme_support( 'custom-logo', array(
-		'height'      => 72,
-		'width'       => 162,
-		'flex-height' => false,
-		'flex-width'  => false,
-		'header-text' => array( 'site-title', 'site-description' ),
-	) );
-
 	/* This theme supports to JetPack's infinite scroll. */
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'articles',
-		'footer'    => false,
-		'wrapper'   => false,
-	) );
+	add_theme_support(
+		'infinite-scroll',
+		array(
+			'container' => 'articles',
+			'footer'    => false,
+			'wrapper'   => false,
+		)
+	);
 
-	/* This theme supports to style the visual editor. */
+	/** This theme supports to style the visual editor. */
 	add_theme_support( 'editor-styles' );
 	add_editor_style( get_template_directory_uri() . '/style-editor.css' );
 }
 
+/**
+ * Widgets initialize timing process.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/widgets_init/
+ */
 function register_sketchpad_widgets() {
-	/* This theme support widget sidebar. */
-	register_sidebar( array(
-		'name'          => __( 'Right Sidebar Widget', 'sketchpad-modified' ),
-		'id'            => 'sidebar',
-		'description'   => __( 'Right Widget Area', 'sketchpad-modified' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<header><h4 class="widgettitle">',
-		'after_title'   => '</h4></header>',
-	) );
+	/** This theme support widget sidebar. */
+	register_sidebar(
+		array(
+			'name'          => __( 'Right Sidebar Widget', 'sketchpad-modified' ),
+			'id'            => 'sidebar',
+			'description'   => __( 'Right Widget Area', 'sketchpad-modified' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<header><h4 class="widgettitle">',
+			'after_title'   => '</h4></header>',
+		)
+	);
 }
 
-/* Load scripts and styles */
+/**
+ * Scripts and styles enqueue process.
+ *
+ * @see https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+ */
 function sketchpad_script() {
-	wp_enqueue_style( 'google-open-sans', '//fonts.googleapis.com/css?family=Open+Sans', array() );
-	wp_enqueue_style( 'sketchpad-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style(
+		'google-open-sans',
+		'//fonts.googleapis.com/css?family=Open+Sans',
+		array()
+	);
+
+	wp_enqueue_style(
+		'sketchpad-style',
+		get_template_directory_uri() . '/style.css',
+		array(),
+		(string) filemtime( get_template_directory() . '/style.css' ),
+		'all'
+	);
 
 	if ( is_singular() ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	/* Auto calculate height */
-	wp_enqueue_script( 'sketchpad-script', get_template_directory_uri() . '/js/sketchpad-ready.js', array( 'jquery' ), false, true );
+	wp_enqueue_script(
+		'sketchpad-script',
+		get_template_directory_uri() . '/js/sketchpad-ready.js',
+		array( 'jquery' ),
+		(string) filemtime( get_template_directory() . '/js/sketchpad-ready.js' ),
+		true
+	);
 }
 
-/* Load scripts and styles for admin pages */
+/**
+ * Scripts and styles enqueue process for admin.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/admin_enqueue_scripts/
+ */
 function sketchpad_admin_script() {
-	wp_enqueue_style( 'sketchpad-admin-style', get_template_directory_uri() . '/style-admin.css' );
+	wp_enqueue_style(
+		'sketchpad-admin-style',
+		get_template_directory_uri() . '/style-admin.css',
+		array(),
+		(string) filemtime( get_template_directory() . '/style-admin.css' ),
+		'all'
+	);
 }
 
-/* style for site title */
+/**
+ * Style for site title.
+ */
 function sketchpad_header() { ?>
 	<style type="text/css">
 		<?php if ( ! display_header_text() ) { ?>
-			.site-title,
-			.site-description {
+			div#main-content > div#wrapper > header .header h1.site-title,
+			div#main-content > div#wrapper > header .header h2.site-description {
 				display: none;
 			}
 		<?php } else { ?>
@@ -125,7 +188,8 @@ function sketchpad_header() { ?>
 			}
 		<?php } ?>
 	</style>
-<?php }
+	<?php
+}
 
 add_action( 'init', 'sketchpad_init' );
 add_action( 'after_setup_theme', 'sketchpad_setup' );
