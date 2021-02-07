043f95be96cf70cddd8ca134938f2143c960ac1d
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-02-07 00:28:23 +0000

    Modified to comply with WordPress conventions.

--- includes/filter/sm-dropdown-categories.php
+++ includes/filter/sm-dropdown-categories.php
@@ -1,25 +1,37 @@
 <?php
 /**
- * Method to extend wp_dropdown_categories.
+ * Extend method.
  *
  * @package sketchpad - modified
  * @subpackage filter
  * @since 1.0.0
  */
 
-function sm_dropdown_categories( $output, $args ){
-  if( isset( $args['sketchpad_multiple'] ) && $args['sketchpad_multiple'] ){
-    $after = '<select multiple'
-              . ( (isset( $args['sketchpad_size'] ) ) ? ' size="' . $args['sketchpad_size'] . '"' : '' );
-    $output = str_replace( '<select', $after, $output );
+/**
+ * Select list categories.
+ *
+ * @param String $output HTML output.
+ * @param Array  $parsed_args Arguments used to build the drop-down.
+ * @return String Converted HTML.
+ * @see https://developer.wordpress.org/reference/hooks/wp_dropdown_cats/
+ */
+function sm_dropdown_categories( $output, $parsed_args ) {
+	if ( isset( $parsed_args['sketchpad_multiple'] ) && $parsed_args['sketchpad_multiple'] ) {
+		$after    = '<select multiple'
+					. ( ( isset( $parsed_args['sketchpad_size'] ) ) ? ' size="' . $parsed_args['sketchpad_size'] . '"' : '' );
+		$output   = str_replace( '<select', $after, $output );
+		$selected = is_array( $parsed_args['sketchpad_selected'] )
+					? $parsed_args['sketchpad_selected']
+					: explode(
+						',',
+						$parsed_args['sketchpad_selected']
+					);
+		foreach ( array_map( 'trim', $selected ) as $value ) {
+			$output = str_replace( "value=\"{$value}\"", "value=\"{$value}\" selected", $output );
+		}
+	}
 
-    $selected = is_array( $args['sketchpad_selected'] ) ? $args['sketchpad_selected'] : explode( ",", $args['sketchpad_selected'] );
-    foreach( array_map( 'trim', $selected ) as $value ){
-      $output = str_replace( "value=\"{$value}\"", "value=\"{$value}\" selected", $output );
-    }
-  }
-
-  return $output;
+	return $output;
 }
 
 add_filter( 'wp_dropdown_cats', 'sm_dropdown_categories', 10, 2 );
--- includes/filter/sm-tag-cloud.php
+++ includes/filter/sm-tag-cloud.php
@@ -1,15 +1,21 @@
 <?php
 /**
- * Method to extend wp_tag_cloud.
+ * Extend method.
  *
  * @package sketchpad - modified
  * @subpackage filter
  * @since 1.0.0
  */
 
-function sm_tag_cloud( $output ){
-  $output = preg_replace( '/\s*?style="[^"]+?"/i', '', $output );
-  return $output;
+/**
+ * The style attribute is removed
+ *
+ * @param String|Array $return Tag cloud as a string or an array, depending on 'format' argument.
+ * @return String|Array If $return is a String, then the converted HTML.
+ * @see https://developer.wordpress.org/reference/hooks/wp_tag_cloud/
+ */
+function sm_tag_cloud( $return ) {
+	return preg_replace( '/\s*?style="[^"]+?"/i', '', $return );
 }
 
 add_filter( 'wp_tag_cloud', 'sm_tag_cloud', 10 );
