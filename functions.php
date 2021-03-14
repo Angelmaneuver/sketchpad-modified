f3466e8ac87c5661d8280f0ee54fd7738ec305af
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-03-14 22:06:31 +0900

    Support for Esperanza.

--- functions.php
+++ functions.php
@@ -99,7 +99,7 @@ function sketchpad_setup() {
 
 	/** This theme supports to style the visual editor. */
 	add_theme_support( 'editor-styles' );
-	add_editor_style( get_template_directory_uri() . '/style-editor.css' );
+	add_editor_style( 'style-editor.css' );
 }
 
 /**
