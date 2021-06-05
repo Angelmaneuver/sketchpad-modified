df3b09e6c30f759f83c93fd06b3d4ad9f3dad99b
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-06-05 14:23:56 +0900

    Addressing coding conventions.

--- 404.php
+++ 404.php
@@ -2,19 +2,20 @@
 /**
  * The template for displaying 404 pages (Not Found)
  *
- * @subpackage Sketchpad
- * @since      Sketchpad 1.7
+ * @package sketchpad
+ * @since   1.7
  */
 
 get_header(); ?>
 	<main>
 		<article <?php post_class(); ?>>
 			<header>
-				<h3 class="post-title"><?php _e( 'Page not Found...', 'sketchpad-modified' ); ?></h3>
+				<h3 class="post-title"><?php esc_html_e( 'Page not Found...', 'sketchpad-modified' ); ?></h3>
 			</header>
-			<p><?php _e( 'Try using the search form', 'sketchpad-modified' ); ?></p>
+			<p><?php esc_html_e( 'Try using the search form', 'sketchpad-modified' ); ?></p>
 			<?php echo get_search_form(); ?>
 		</article>
 	</main>
-<?php get_sidebar();
+<?php
+get_sidebar();
 get_footer();
