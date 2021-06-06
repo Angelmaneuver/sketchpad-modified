bbd92f6bf6df2c743b15b62bdf60359b2579ad4c
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-06-06 13:12:36 +0900

    Code review and Addressing coding conventions.

--- header.php
+++ header.php
@@ -2,9 +2,10 @@
 /**
  * The Header template for our theme
  *
- * @subpackage Sketchpad
- * @since      Sketchpad 1.7
+ * @package sketchpad
+ * @since   1.7.0
  */
+
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -12,8 +13,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) { ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php }
-	wp_head(); ?>
+		<?php
+	}
+	wp_head();
+	?>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
@@ -22,25 +25,31 @@
 		<header>
 			<div class="logo">
 				<?php if ( get_header_image() ) { ?>
-					<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-				<?php }
+					<img src="<?php header_image(); ?>" height="<?php echo esc_html( get_custom_header()->height ); ?>" width="<?php echo esc_html( get_custom_header()->width ); ?>" alt="" />
+					<?php
+				}
 				if ( function_exists( 'the_custom_logo' ) ) {
 					if ( has_custom_logo() ) {
 						the_custom_logo();
 					}
 				} elseif ( get_theme_mod( 'sketchpad_logo' ) ) {
 					echo '<a href="' . esc_url( home_url( '/' ) ) . '"><img src="' . esc_url( get_theme_mod( 'sketchpad_logo' ) ) . '" alt="logotype" title="Back home" /></a>';
-				} ?>
+				}
+				?>
 			</div><!--.logo-->
 			<div class="header">
 				<h1 class="site-title">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Home" style="color: #<?php header_textcolor(); ?>"><?php bloginfo( 'name' ); ?></a>
 				</h1>
-				<?php $description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) { ?>
-					<h2 class="site-description"><?php echo $description; ?></h2>
-				<?php }
-				echo get_search_form(); ?>
+				<?php
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) {
+					?>
+					<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
+					<?php
+				}
+				echo get_search_form();
+				?>
 			</div><!--.header-->
 		</header>
 		<div class="content">
@@ -49,4 +58,5 @@
 					<nav>
 						<div class="main-navigation"><?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></div>
 					</nav>
-				<?php }
+					<?php
+				}
