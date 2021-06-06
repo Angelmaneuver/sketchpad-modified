79e548a6e8dbfeb460037f9aa9612b44e9a0b30d
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-06-06 22:44:50 +0900

    Refactoring as pointed out by Code Climate.

---
+++ template/article.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * The template for displaying the other type content
+ *
+ * @package sketchpad
+ * @since   2.1.0
+ */
+
+?>
+<article <?php post_class(); ?>>
+	<header>
+		<?php
+		if ( is_singular() ) {
+			do_action( 'sketchpad_modified_breadcrumb' );
+		}
+		?>
+		<div class="post-header">
+			<h3 class="post-title">
+				<?php
+				if ( is_singular() ) {
+					the_title();
+					echo '<span class="r18 fa-stack"><i class="fas fa-ban fa-stack-2x"></i><span class="fa-stack-1x">18</span></span>';
+				} else {
+					the_esc_html_a( '<a href="' . get_permalink() . '">' . get_the_title() . '<span class="r18 fa-stack"><i class="fas fa-ban fa-stack-2x"></i><span class="fa-stack-1x">18</span></span></a>' );
+					the_esc_html_a( '<a class="external" href="' . get_permalink() . '" rel="noopener" target="_blank"></a>' );
+				}
+				?>
+			</h3>
+		</div><!--.post-header-->
+		<div class="post-meta">
+			<span>
+				<time class="post-date" datetime="<?php the_time( 'Y-m-d' ); ?>">
+					<?php
+					if ( is_singular() ) {
+						printf( '<a href="%1$s" title="%2$s">%3$s</a>', esc_url( ( is_singular() ) ? get_month_link( get_the_time( 'Y' ), get_the_time( 'm' ) ) : get_the_permalink() ), the_title_attribute( 'echo=0' ), get_the_date() );
+					} else {
+						the_esc_html_a( '<a href="' . get_permalink() . '">' . get_the_date() . '</a>' );
+					}
+					?>
+				</time>
+				<?php edit_post_link( __( 'edit', 'sketchpad-modified' ) ); ?>
+			</span>
+			<span class="by-author">
+				<?php
+				esc_html_e( 'posted by ', 'sketchpad-modified' );
+				the_author_posts_link();
+				if ( has_category() ) {
+					esc_html_e( ' in ', 'sketchpad-modified' );
+					the_category( ', ' );
+				}
+				?>
+			</span>
+		</div><!--.post-meta-->
+	</header>
+	<div class="post-content">
+		<?php
+		if ( is_singular() ) {
+			the_content();
+		} else {
+			if ( $args['excerpt'] ) {
+				the_excerpt();
+			} else {
+				the_content();
+			}
+		}
+		?>
+	</div><!--.post-content-->
+	<footer>
+		<?php
+		/* post pagination */
+		wp_link_pages(
+			array(
+				'before' => '<div class="pagination">',
+				'after'  => '</div>',
+			)
+		);
+		if ( ! is_singular() ) {
+			?>
+			<div class="post-footer">
+				<span>
+					<?php if ( get_comments_number() > 0 ) { ?>
+						<a class="comments-link" href="<?php the_permalink(); ?>#comments">
+						<?php
+						esc_html_e( 'read comments', 'sketchpad-modified' );
+						echo esc_html( ' (' . ( get_comments_number() ) . ')' );
+						?>
+						</a>
+					<?php } ?>
+				</span>
+				<span>
+					<?php if ( $args['excerpt'] ) { ?>
+						<a class="more-link" href="<?php the_permalink(); ?>"><?php esc_html_e( 'more', 'sketchpad-modified' ); ?> &raquo;</a>
+					<?php } ?>
+				</span>
+			</div><!--.post-footer-->
+			<?php
+		}
+		if ( has_tag() ) { /* display if has tags */
+			echo '<div class="post-tag">';
+			the_tags( __( 'Tags: ', 'sketchpad-modified' ), ' ' );
+			echo '</div>';
+		}
+		?>
+	</footer>
+</article>
