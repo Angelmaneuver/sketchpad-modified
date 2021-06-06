8d7c04455c746fe1c568f04055a3da85fa3187ab
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-06-06 13:47:50 +0900

    Code review and Addressing coding conventions.

--- index.php
+++ index.php
@@ -6,21 +6,25 @@
  * two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  *
- * @subpackage Sketchpad
- * @since      Sketchpad 1.7
+ * @package sketchpad
+ * @since   1.7.0
  */
 
 get_header(); ?>
 	<main id="articles">
-		<?php if ( have_posts() ) :
+		<?php
+		if ( have_posts() ) :
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'content', get_post_format() );
-			endwhile; ?>
+			endwhile;
+			?>
 			<div class="pagination"><!--page pagination-->
-				<?php global $wp_query;
-				$big = 999999999; // need an unlikely integer
-				echo paginate_links( array(
+				<?php
+				global $wp_query;
+				$big = 999999999; // need an unlikely integer.
+				the_paginate_links(
+					array(
 						'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 						'format'    => '?paged=%#%',
 						'current'   => max( 1, get_query_var( 'paged' ) ),
@@ -28,17 +32,19 @@ get_header(); ?>
 						'prev_text' => '&laquo;',
 						'next_text' => '&raquo;',
 					)
-				); ?>
+				);
+				?>
 			</div><!--.pagination-->
 		<?php else : ?>
 			<article <?php post_class(); ?>>
 				<header>
-					<h3 class="post-title"><?php _e( 'Not Found...', 'sketchpad-modified' ); ?></h3>
+					<h3 class="post-title"><?php esc_html_e( 'Not Found...', 'sketchpad-modified' ); ?></h3>
 				</header>
-				<p><?php _e( 'Try using the search form', 'sketchpad-modified' ); ?></p>
+				<p><?php esc_html_e( 'Try using the search form', 'sketchpad-modified' ); ?></p>
 				<?php echo get_search_form(); ?>
 			</article>
-		<?php endif; /*have_posts*/ ?>
+		<?php endif; /* have_posts */ ?>
 	</main>
-<?php get_sidebar();
+<?php
+get_sidebar();
 get_footer();
