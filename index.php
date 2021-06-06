03d23119a1a20e12937c56d26da773e5e79cc1f4
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-06-06 15:58:44 +0900

    Refactoring as pointed out by Code Climate.

--- index.php
+++ index.php
@@ -13,37 +13,15 @@
 get_header(); ?>
 	<main id="articles">
 		<?php
-		if ( have_posts() ) :
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'content', get_post_format() );
-			endwhile;
-			?>
-			<div class="pagination"><!--page pagination-->
-				<?php
-				global $wp_query;
-				$big = 999999999; // need an unlikely integer.
-				the_paginate_links(
-					array(
-						'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-						'format'    => '?paged=%#%',
-						'current'   => max( 1, get_query_var( 'paged' ) ),
-						'total'     => $wp_query->max_num_pages,
-						'prev_text' => '&laquo;',
-						'next_text' => '&raquo;',
-					)
-				);
-				?>
-			</div><!--.pagination-->
-		<?php else : ?>
-			<article <?php post_class(); ?>>
-				<header>
-					<h3 class="post-title"><?php esc_html_e( 'Not Found...', 'sketchpad-modified' ); ?></h3>
-				</header>
-				<p><?php esc_html_e( 'Try using the search form', 'sketchpad-modified' ); ?></p>
-				<?php echo get_search_form(); ?>
-			</article>
-		<?php endif; /* have_posts */ ?>
+		get_template_part(
+			'template/articles',
+			'',
+			array(
+				'not_articles_post_title' => __( 'Not Found...', 'sketchpad-modified' ),
+				'not_articles_message'    => __( 'Try using the search form', 'sketchpad-modified' ),
+			)
+		);
+		?>
 	</main>
 <?php
 get_sidebar();
