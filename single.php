a4bcf192da6e596f8b47e2843a4d091cf3f5eeee
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-06-06 14:38:46 +0900

    Code review and Addressing coding conventions.

--- single.php
+++ single.php
@@ -2,20 +2,26 @@
 /**
  * The template for displaying all single posts
  *
- * @subpackage Sketchpad
- * @since      Sketchpad 1.7
+ * @package sketchpad
+ * @since   1.7.0
  */
 
 get_header(); ?>
 	<main>
-		<?php while ( have_posts() ) : the_post();
-			get_template_part( 'content', get_post_format() ); ?>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'content', get_post_format() );
+			?>
 			<div class="post-navigation">
 				<div class="prev-post"><?php previous_post_link( '<span>&larr;</span><span>%link</span>' ); ?></div><!--Previous post navigation-->
 				<div class="next-post"><?php next_post_link( '<span>&rarr;</span><span>%link</span>' ); ?></div><!--Next post navigation-->
 			</div>
-			<?php comments_template();
-		endwhile; ?>
+			<?php
+			comments_template();
+		endwhile;
+		?>
 	</main>
-<?php get_sidebar();
+<?php
+get_sidebar();
 get_footer();
