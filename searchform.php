f43eae981f7f83271355816bbb020912c62dae06
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-06-06 14:07:03 +0900

    Code review and Addressing coding conventions.

--- searchform.php
+++ searchform.php
@@ -2,12 +2,12 @@
 /**
  * The template for displaying search form
  *
- * @subpackage Sketchpad
- * @since      Sketchpad 1.9
+ * @package sketchpad
+ * @since   1.9.0
  */
-?>
 
+?>
 <form class="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input class="search-text" size="17" type="search" name="s" placeholder="<?php esc_attr_e( 'Enter search keyword', 'sketchpad-modified' ) ?>" value="<?php the_search_query(); ?>">
-	<input class="search-button" type="submit" value="<?php _e( 'search', 'sketchpad-modified' ) ?>">
+	<input class="search-text" size="17" type="search" name="s" placeholder="<?php esc_attr_e( 'Enter search keyword', 'sketchpad-modified' ); ?>" value="<?php the_search_query(); ?>">
+	<input class="search-button" type="submit" value="<?php esc_html_e( 'search', 'sketchpad-modified' ); ?>">
 </form>
