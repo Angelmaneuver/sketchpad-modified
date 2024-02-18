d662d8adacbb5092491eb176a857262c38816528
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2024-02-19 00:04:31 +0900

    Added the indent styles.

--- includes/block/style/core/paragraph.php
+++ includes/block/style/core/paragraph.php
@@ -10,6 +10,8 @@
 // @codingStandardsIgnoreStart
 return [
 	[ 'name' => 'text-indent',                   'label' => __( 'Single character indentation',    'sketchpad-modified' ) ],
+	[ 'name' => 'text-indent-two-letters',       'label' => __( 'Two character indentation',       'sketchpad-modified' ) ],
+	[ 'name' => 'text-indent-three-letters',     'label' => __( 'Three character indentation',     'sketchpad-modified' ) ],
 	[ 'name' => 'single-character-line-spacing', 'label' => __( 'Open one character between line', 'sketchpad-modified' ) ],
 	[ 'name' => 'speech',                        'label' => __( 'Speech',                          'sketchpad-modified' ) ],
 	[ 'name' => 'r18-paragraph',                 'label' => __( 'Change the display to R18.',      'sketchpad-modified' ) ],
